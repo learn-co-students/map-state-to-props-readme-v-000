8366fc1afed4c39220c47a1dff5e10ffbc47f515
Canino Developments <ian.canino@gmail.com>
2018-07-25 15:50:31 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
+import { connect } from 'react-redux' /* code change */
+import './App.css'
+ 
 class App extends Component {
-
-  handleOnClick() {
+  handleOnClick () {
     this.props.store.dispatch({
-      type: 'INCREASE_COUNT',
-    });
+      type: 'INCREASE_COUNT'
+    })
   }
 
-  render() {
+  render () {
     return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
+      <div className='App'>
+        <button onClick={() => this.handleOnClick()}>
           Click
         </button>
         <p>{this.props.items.length}</p>
       </div>
-    );
+    )
   }
 };
 
-export default App;
+// start of code change
+const mapStateToProps = (state) => {
+  return { items: state.items }
+};
+
+export default connect(mapStateToProps)(App)
+// end of code change
