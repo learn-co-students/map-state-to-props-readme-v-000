721c4518cb53ee1be404e4063b3f03a08be2ceb6
kristinreddington <kristinreddington@gmail.com>
2018-08-01 23:34:28 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { connect } from 'react-redux'
 class App extends Component {
 
   handleOnClick() {
@@ -21,4 +21,8 @@ class App extends Component {
   }
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+
+export default connect(mapStateToProps)(App);
