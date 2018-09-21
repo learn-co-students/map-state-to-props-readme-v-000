edbbdcc7fbd0f8b17de3a0d8e2fdaa40a38a6c08
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-09-21 10:59:42 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
 
 const store = createStore(
   shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(
-  <App store={store}/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
