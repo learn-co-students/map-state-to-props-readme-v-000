555608d73b28c4b321471e780a4b448624d0ff04
FionaDL <github email address>
2019-08-08 10:22:44 +0200

    Done.

--- src/App.js
+++ src/App.js
@@ -22,4 +22,12 @@ class App extends Component {
   }
 };
 
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+
+export default connect(mapStateToProps)(App);
+
 export default App;
+
+...
