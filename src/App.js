cfcf6203f87e279f804d5685da5ad921a90d6bf5
yuanxizhang <yuanxi.zhang@gmail.com>
2020-07-19 13:02:51 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
 import './App.css';
-
+ 
 class App extends Component {
-
+ 
   handleOnClick() {
     this.props.dispatch({
       type: 'INCREASE_COUNT',
     });
   }
-
+ 
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
+        <button onClick={() => this.handleOnClick()}>
           Click
         </button>
         <p>{this.props.items.length}</p>
@@ -20,5 +21,10 @@ class App extends Component {
     );
   }
 };
-
-export default App;
+ 
+// slice of the state to be conencted to the app component
+const mapStateToProps = (state) => {
+  return { items: state.items };
+};
+ 
+export default connect(mapStateToProps)(App);
